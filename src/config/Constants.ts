c51fcabfb1fb3890eb0687ac2ad9d3e98ff23a2d
Nara <51725846+narateracin@users.noreply.github.com>
2022-07-08 20:09:54 -0300

    Update Constants.ts

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -1,4 +1,4 @@
 export const config = {
     API_URL: 'http://localhost:5000',
-    MONGO_CONNECTION: 'mongodb+srv://localhost:newstaffs89@url-shortener-dio.ckrhz.mongodb.net/?retryWrites=true&w=majority',
+    MONGO_CONNECTION: 'mongodb+srv://localhost:<password>@url-shortener-dio.ckrhz.mongodb.net/?retryWrites=true&w=majority',
 }
